66b299ea89c3a678cc6c20562da304c54eb1cfaa
Maritza <maritzasintitafur@gmail.com>
2016-08-19 11:37:51 -0500

    Clase 4: Método Math e intro a JSON

--- js-04/04-math.js
+++ js-04/04-math.js
@@ -4,10 +4,10 @@
     Math es un objeto del lenguaje JavaScript que permite trabajar con números.
   */
 
-  console.log(Math.E);
+  console.log(Math.E); //epsilon
   console.log(Math.PI);
 
-  // .ceil() redondea un numero hacia arriba
+  // El método .ceil() redondea un numero hacia arriba
   var numero = 3.2;
   console.log(Math.ceil(numero));
   // .floor() redondea hacia abajo
@@ -16,8 +16,10 @@
   console.log(Math.round(numero));
   numero = 3.5;
   console.log(Math.round(numero));
-  // .max() y .min() que devuelven el mayor y menor valor de una lista de parámetros
+  // .max() y .min() que devuelven el mayor y menor valor de un set(lista) de parámetros
+  console.log(Math.max(2,6,8,123));
   // sqrt() devuelve la raiz cuadrada
+  // abs() devuelve el valor absoluto
   console.log(Math.sqrt(49));
   // .random() genera un numero aleatorio entre 0 y 1
   console.log(Math.random());
@@ -29,3 +31,23 @@
   console.log(Math.floor(Math.random() * (max - min)) + min);
 
 })();
+
+/*
+JSON JavaScript Object Notation
+
+- Es una notación estructural de información o para estructural info. Está inspirada en JS pero no es JS. 
+- Su estructura es: "Clave": Valor,
+- Se usa para compartir información. No es programación!!
+- Existen DB como MongoDb que usan JSON
+- Aunque está basado en los objetos de Js tiene dos diferencias fundamentales con estos:
+  1. Todas las claves van entrecomilladas
+  2. No existen métodos, solo propiedades. Pues solo sirve para compartir info.
+/*
+
+/*
+OJO:
+  Los métodos llevan parentesis. Las propiedades no. 
+  Los métodos son las funciones que realiza un objeto. Las propiedades son las características de ese objeto.
+
+  Por esa razón aunque hay un cálculo de por medio, .lenght no es un método, es una propiedad. :D
+/*
